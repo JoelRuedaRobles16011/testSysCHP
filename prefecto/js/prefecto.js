34b1a0e458fa518fb322a4c23e2047e5f737f48f
Joel <joel.rueda.robles.sc@gmail.com>
2019-12-25 19:39:36 -0600

    Se removieron los preloaders de cada formulario

--- prefecto/js/prefecto.js
+++ prefecto/js/prefecto.js
@@ -8,15 +8,6 @@ document.querySelector('#anclaJustificante').addEventListener('click', function 
 
     xhttp.onreadystatechange = function () {
 
-        if (this.readyState == 1) {
-            document.querySelector('main').innerHTML = /* html */
-            `
-            <div class="spinner-border text-primary" role="status">
-                <span class="sr-only">Loading...</span>
-            </div>
-            `;
-        }
-
         if (this.readyState == 4 && this.status == 200) {
             document.querySelector('main').innerHTML = this.responseText;
         }
@@ -32,15 +23,6 @@ document.querySelector('#anclaReporte').addEventListener('click', function () {
     xhttp.send();
 
     xhttp.onreadystatechange = function () {
-
-        if (this.readyState == 1) {
-            document.querySelector('main').innerHTML = /* html */
-            `
-            <div class="spinner-border text-primary" role="status">
-                <span class="sr-only">Loading...</span>
-            </div>
-            `;
-        }
 
         if (this.readyState == 4 && this.status == 200) {
             document.querySelector('main').innerHTML = this.responseText;
@@ -58,15 +40,6 @@ document.querySelector('#anclaCitatorio').addEventListener('click', function () 
 
     xhttp.onreadystatechange = function () {
 
-        if (this.readyState == 1) {
-            document.querySelector('main').innerHTML = /* html */
-            `
-            <div class="spinner-border text-primary" role="status">
-                <span class="sr-only">Loading...</span>
-            </div>
-            `;
-        }
-        
         if (this.readyState == 4 && this.status == 200) {
             document.querySelector('main').innerHTML = this.responseText;
         }
