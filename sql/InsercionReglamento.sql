828def4a48b5edd9d519bb99c56e03665f3c56bf
Joel <joel.rueda.robles.sc@gmail.com>
2019-12-26 17:21:05 -0600

    Base de datos para el reglamento

---
+++ sql/InsercionReglamento.sql
@@ -0,0 +1,85 @@
+USE SYSCHP;
+
+INSERT INTO CLAUSULA VALUES
+    (4, 'Faltas leves'),
+    (5, 'Flatas graves')
+;
+
+INSERT INTO ARTICULO VALUES
+    (55, 4, 'Falta leve: Es una infracción a la reglamentación institucional que se hace de manera espontánea, sin dolo y que no atenta contra la integridad física, psicológica y moral de las personas y/o bienes de la escuela. Se sanciona con un reporte disciplinario. La reincidencia en este tipo de faltas se considerará falta grave.'),
+    (56, 4, 'Son faltas leves las siguientes:'),
+    (57, 5, 'Las siguientes son faltas graves y se sancionan con suspensión temporal por 5 días y carta de condicionamiento:'),
+    (58, 5, 'Las siguientes son faltas graves y se sancionan con suspensión temporal hasta por 10 días y carta de condicionamiento:'),
+    (59, 5, 'Las siguientes son faltas graves y se sancionan con suspensión definitiva:')
+;
+
+INSERT INTO REGLA VALUES
+    (1, 56, 'La interrupción de clases en los siguientes casos: hacer ruidos estridentes, gritar, silbar, provocar desorden, jugar o practicar actos similares dentro y fuera del salón.'),
+    (2, 56, 'Jugar en la cancha o en otras áreas en horarios no permitidos.'),
+    (3, 56, 'Introducir y/o consumir alimentos o bebidas al salón de clases, sala audiovisual, laboratorio de cómputo, biblioteca y laboratorio de ciencias.'),
+    (4, 56, 'El acudir al plantel sin uniforme o con el uniforme incompleto, cambiarse o modificar el uniforme dentro de las instalaciones.'),
+    (5, 56, 'Daños a las áreas verdes. Esta falta, además de ser sancionada con reporte amerita la reparación del daño.'),
+    (6, 56, 'El uso de lenguaje altisonante y/ o actos y señas obscenas dentro de la escuela.'),
+    (7, 56, 'La planeación y el abandono de clases sin autorización del maestro. Esta falta ameritará además el registro de doble inasistencia a la clase.'),
+    (8, 56, 'Manifestaciones de noviazgo dentro de la escuela y sus alrededores en el horario de clases.'),
+    (9, 56, 'No ingresar a clases estando en el plantel.'),
+    (10, 56, 'El uso de prendas ajenas al uniforme y accesorios extravagantes, mismos que al ser retenidos, se devolverán al final del semestre.'),
+    (11, 56, 'Tirar basura en aulas, patios, jardines, etc. Esta falta, además de ser sancionada con reporte amerita multa económica o en especie, o trabajos de aseo.'),
+    (12, 56, 'Portar el uniforme de educación física cuando no corresponda.'),
+    (13, 56, 'Introducir al plantel cerillos, encendedores, o cualquier material que pueda provocar fuego. Esta falta, además de ser sancionada con reporte, se confiscará los objetos no permitidos.'),
+    (14, 56, 'El uso do maquillaje exagerado en las mujeres, como el maquillarse en el aula de clases, En esta falta, además de ser sancionada con reporte, la alumna tendrá que desmaquillarse.'),
+    (15, 56, 'Obstruir los pasillos, escaleras y/o salidas realizando actividades escolares o comiendo.'),
+    (16, 56, 'Ensuciar paredes, ventanas, techos, mobiliario, puertas y pizarrones, subiendo los pies o poniendo las manos sucias en cualquier espacio del inmueble.'),
+    (17, 56, 'Uso de celular, laptop o cualquier aparato electrónico en el aula, En esta falta so confiscará el aparato y se entregará al tutor, al final del ciclo.'),
+    (18, 56, 'Encender aparatos electrónicos de sonido, de video, o hacer sonar instrumentos musicales. Se excluyen los ensayos, prácticas o actividades artísticas.'),
+    (19, 56, 'Ingresar al plantel en el caso de los varones, con el pelo largo que rebase la parte superior de la oreja, con cortes, peinados o tintes extravagantes, o con trenzas, chongos o colas.'),
+    (20, 56, 'Ingresar al plantel en el caso de las mujeres, con cortes, peinados o tintes extravagantes, así como la falda demasiado corta y blusas escotadas.'),
+    (21, 56, 'El uso de juegos de Azar dentro del plantel, o en actividades extra escolares.'),
+    (22, 56, 'Estacionarse en lugares prohibidos, ya sea dentro del espacio del inmueble escolar designado o de su periferia, o en aquellos que ocasionen problemas viales o dificultades con los vecinos de dichos espacios.'),
+    (23, 56, 'Encender, dentro del espacio escolar, cualquier tipo de aparatos receptores y reproductores de sonidos y videos, hacer sonar o tocar instrumentos musicales. Se excluyen los ensayos para las actividades artísticas, prácticas o función, si se efectúan en los escenarios a ellos destinados.'),
+
+
+    (1, 57, ''),
+    (2, 57, ''),
+    (3, 57, ''),
+    (4, 57, ''),
+    (5, 57, ''),
+    (6, 57, ''),
+    (7, 57, ''),
+    (8, 57, ''),
+    (9, 57, ''),
+    (10, 57, ''),
+    (11, 57, ''),
+    (12, 57, ''),
+    (13, 57, ''),
+    (14, 57, ''),
+
+
+    (1, 58, ''),
+    (2, 58, ''),
+    (3, 58, ''),
+    (4, 58, ''),
+    (5, 58, ''),
+    (6, 58, ''),
+    (7, 58, ''),
+    (8, 58, ''),
+    (9, 58, ''),
+    (10, 58, ''),
+    (11, 58, ''),
+    (12, 58, ''),
+
+
+    (1, 59, ''),
+    (2, 59, ''),
+    (3, 59, ''),
+    (4, 59, ''),
+    (5, 59, ''),
+    (6, 59, ''),
+    (7, 59, ''),
+    (8, 59, ''),
+    (9, 59, ''),
+    (10, 59, ''),
+    (11, 59, ''),
+    (12, 59, ''),
+    (13, 59, '')
+;
