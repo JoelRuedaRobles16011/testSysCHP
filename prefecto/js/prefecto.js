4269e89aad3dccc8e7442adbbf104c24eed6aa61
Joel <joel.rueda.robles.sc@gmail.com>
2019-12-25 19:23:28 -0600

    Formularios Responsivos

--- prefecto/js/prefecto.js
+++ prefecto/js/prefecto.js
@@ -8,7 +8,7 @@ document.querySelector('#anclaJustificante').addEventListener('click', function 
 
     xhttp.onreadystatechange = function () {
 
-        if (this.readyState == 2) {
+        if (this.readyState == 1) {
             document.querySelector('main').innerHTML = /* html */
             `
             <div class="spinner-border text-primary" role="status">
@@ -32,6 +32,16 @@ document.querySelector('#anclaReporte').addEventListener('click', function () {
     xhttp.send();
 
     xhttp.onreadystatechange = function () {
+
+        if (this.readyState == 1) {
+            document.querySelector('main').innerHTML = /* html */
+            `
+            <div class="spinner-border text-primary" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+            `;
+        }
+
         if (this.readyState == 4 && this.status == 200) {
             document.querySelector('main').innerHTML = this.responseText;
         }
@@ -47,6 +57,16 @@ document.querySelector('#anclaCitatorio').addEventListener('click', function () 
     xhttp.send();
 
     xhttp.onreadystatechange = function () {
+
+        if (this.readyState == 1) {
+            document.querySelector('main').innerHTML = /* html */
+            `
+            <div class="spinner-border text-primary" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+            `;
+        }
+        
         if (this.readyState == 4 && this.status == 200) {
             document.querySelector('main').innerHTML = this.responseText;
         }
